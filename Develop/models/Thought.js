c12a7f22e87ca1afb71234f12fb1878bec7be107
NicoleWrz <nicolew96@gmail.com>
2022-01-05 20:34:59 -0500

    creating models

---
+++ Develop/models/Thought.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+const dateFormat = require('../utils/dateFormat');
+
+const thoughtSchema = new Schema(
+  {
+    // TODO: add thoughtText
+    thoughtText: {
+      type: String,
+      required: true,
+      maxlength: 250,
+      minlength: 1,
+    },
+
+    // TODO: add createdAt
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      //Use a getter method to format the timestamp on query
+    },
+
+    // TODO: add username
+    username: {
+    type: String,
+    required: true,
+  },
+    // TODO: add reactions
+    reactions: [reactionSchema]
+
+  },
+  {
+    // TODO: Add toJSON option
+    toJSON: {
+      getters: true
+    }
+  }
+);
+
+thoughtSchema.virtual('reactionCount').get(function() {
+  return this.reactions.length;
+});
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
---
+++ Develop/models/User.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    // TODO: create username field
+    username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+    },
+
+    // TODO: create email field
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/]
+    },
+    
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
